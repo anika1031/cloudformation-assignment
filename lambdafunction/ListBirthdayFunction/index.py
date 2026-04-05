e0a7b0860233afc5bf88ccecfb8910e7c683b01a
anika1031 <anikap2584@gmail.com>
2026-04-05 08:56:06 +0530

    Implement Lambda function to list birthdays from DynamoDB

---
+++ lambdafunction/ListBirthdayFunction/index.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+def lambda_handler(event, context):
+    response = table.scan()
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
+        'body': json.dumps({
+            "birthdays": response.get('Items', [])
+        })
+    }
